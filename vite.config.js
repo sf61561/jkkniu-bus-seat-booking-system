5c0835ffbb86b815e27b3e7ac1dd615fe6d8529f
sf61561 <sf61561@gmail.com>
2025-09-05 22:33:57 +0600

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/jkkniu-bus-seat-booking-system/",
   plugins: [react(),tailwindcss()],
 })
